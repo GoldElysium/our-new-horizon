8b646316c7c5d3b6705b9aa86f04678a6f365896
GoldElysium <goldelysium@gmail.com>
2025-07-07 11:59:03 +0200

    chore: remove sidebar collapse

--- src/components/sidebar.ts
+++ src/components/sidebar.ts
@@ -14,11 +14,6 @@ export class Sidebar extends LitElement {
 		return this;
 	}
 
-	changeCollapse() {
-		this.collapsed = !this.collapsed;
-		this.onCollapse();
-	}
-
 	render() {
 		return html`
 			<div class="relative h-full bg-[#243962]">
@@ -54,46 +49,6 @@ export class Sidebar extends LitElement {
 					></message-display>
 					<volume-control></volume-control>
 				</div>
-				<button
-					class="${this.collapsed ?
-						'slide-out -right-18'
-					:	'slide-in -right-8'} absolute top-12 grid place-items-center rounded-r-full bg-[#243962] py-2 pr-2 pl-1 text-white"
-					@click="${() => this.changeCollapse()}"
-				>
-					${this.collapsed ?
-						html`
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="size-6"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="m8.25 4.5 7.5 7.5-7.5 7.5"
-								/>
-							</svg>
-						`
-					:	html`
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="size-6"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M15.75 19.5 8.25 12l7.5-7.5"
-								/>
-							</svg>
-						`}
-				</button>
 			</div>
 		`;
 	}
